e1d6aab19a2efd809cc912ea3a696f2f3a42a315
masaman <70780836+masavasc@users.noreply.github.com>
2025-11-06 17:43:33 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (<html lang="ja"><body>{children}</body></html>);
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
 }
